b3f667dbd5eb9689b3bf9298302ecc248c988423
laurentvuillaume <figura.graphik@gmail.com>
2026-05-21 13:59:08 +0200

    update: integrate header-page, section-kpis, table-recent-tools components in home page (dashboard)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from "next/image";
+import HeaderPage from "@/components/header-page";
+import SectionKPIs from "@/components/kpis/section-kpis";
+import RecentsTools from "@/components/tools/table-recent-tools";
+import { getRecentToolsForTable } from "@/services/tools.service";
 
-export default function Home() {
+export default async function Home() {
+  const recentTools = (await getRecentToolsForTable()).slice(1, 8);
+  console.log("data recentTools: ", recentTools);
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-0 gap-16">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        APP
-      </main>
+    <div className="flex flex-col items-center justify-center gap-4">
+      <HeaderPage
+        title="Internal Tools Dashboard"
+        subtitle="Monitor and manage tour organization's software tools and expenses"
+      />
+      <section className="flex flex-row max-w-7xl px-0 py-2 w-full">
+        <SectionKPIs />
+      </section>
+
+      <section className="w-full max-w-7xl px-0 py-2">
+        <RecentsTools tools={recentTools} />
+      </section>
     </div>
   );
 }
