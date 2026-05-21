f862d8ccaaf533c40b4ace411220bec1817ecb9f
laurentvuillaume <figura.graphik@gmail.com>
2026-05-21 14:14:28 +0200

    add: create formatCurrency utility function

---
+++ frontend/src/utils/formatCurrency.ts
@@ -0,0 +1,7 @@
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: "EUR",
+    maximumFractionDigits: 2,
+  }).format(value);
+}
