b719300597c43b96858d1c84edfbd97b05ce914e
laurentvuillaume <figura.graphik@gmail.com>
2026-05-25 17:13:30 +0200

    update type for department

--- frontend/src/types/department.ts
+++ frontend/src/types/department.ts
@@ -1,27 +1,9 @@
-export type OutputDepartment = {
-  id: string;
+export type Department = {
+  id: number;
   name: string;
   description: string;
   created_at: string;
   updated_at: string;
 };
 
-export type InputDepartment = {
-  name: string;
-  description: string;
-};
-
-export type DepartmentWithTools = OutputDepartment & {
-  tools: {
-    id: number;
-    name: string;
-    icon_url: string;
-    monthly_cost: number;
-    status: "active" | "unused" | "expiring";
-  }[];
-};
-
-export type KpiDepartment = {
-  id: number;
-  name: string;
-};
+export type ApiJsonDepartment = Partial<Department>;
