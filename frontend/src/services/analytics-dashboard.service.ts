303ef36e7522ba1a575b4fccfdf7ba1dac43209d
laurentvuillaume <figura.graphik@gmail.com>
2026-05-26 06:31:13 +0200

    add analytics-dashboard service

---
+++ frontend/src/services/analytics-dashboard.service.ts
@@ -0,0 +1,62 @@
+import { getAnalytics } from "@/services/analytics.service";
+import { getDepartments } from "@/services/departments.service";
+import { getAllTools } from "@/services/tools.service";
+import { getAllUserTools } from "@/services/user-tools.service";
+
+import {
+  getCostByDepartment,
+  getExpiringTools,
+  getLeastUsedTools,
+  getMostUsedTools,
+  getPotentialSavings,
+  getTopExpensiveTools,
+  getUnusedTools,
+} from "@/utils/analytics";
+
+export async function getAnalyticsDashboardData() {
+  const [analytics, departments, tools, userTools] = await Promise.all([
+    getAnalytics(),
+    getDepartments(),
+    getAllTools(),
+    getAllUserTools(),
+  ]);
+
+  const totalMonthlySpend = analytics.budget_overview.current_month_total;
+
+  const monthlyLimit = analytics.budget_overview.monthly_limit;
+
+  const budgetUtilization = Number(
+    analytics.budget_overview.budget_utilization,
+  );
+
+  const departmentCosts = getCostByDepartment(tools);
+
+  const topExpensiveTools = getTopExpensiveTools(tools);
+
+  const mostUsedTools = getMostUsedTools(tools, userTools);
+
+  const leastUsedTools = getLeastUsedTools(tools, userTools);
+
+  const unusedTools = getUnusedTools(tools);
+
+  const expiringTools = getExpiringTools(tools);
+
+  const potentialSavings = getPotentialSavings(tools);
+
+  return {
+    analytics,
+    departments,
+    tools,
+    userTools,
+    totalMonthlySpend,
+    monthlyLimit,
+    budgetUtilization,
+    departmentCosts,
+    topExpensiveTools,
+    mostUsedTools,
+    leastUsedTools,
+    unusedTools,
+    expiringTools,
+    potentialSavings,
+  };
+}
